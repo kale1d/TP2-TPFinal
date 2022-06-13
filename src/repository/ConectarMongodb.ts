d6b304cfe20bb175ed4bef48ba62d025ddcd046b
Matias Habibo <matihabibo@gmail.com>
2022-06-13 11:22:39 -0300

    prueba mati git

--- src/repository/ConectarMongodb.ts
+++ src/repository/ConectarMongodb.ts
@@ -5,13 +5,13 @@ class ConectarMongodb {
   private dbName = "sistema";
   async conectar() {
     await this.client.connect();
-    console.log("Conectado a base de datos " + this.dbName);
+    console.log("Conectado a base de datos: " + this.dbName);
     const db = this.client.db(this.dbName);
     return db;
   }
   async desconectar() {
     await this.client.close();
-    console.log("Desconectado de la base de datos " + this.dbName);
+    console.log("Desconectado de la base de datos: " + this.dbName);
   }
 }
 export { ConectarMongodb };
